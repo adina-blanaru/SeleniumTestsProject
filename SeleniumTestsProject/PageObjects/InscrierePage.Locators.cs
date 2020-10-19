c7fbe98e363ff1ce5327f6ff60a0acd9f86fa40e
Adina Blanaru <adina.blanaru@gmail.com>
2020-10-19 21:37:01 +0300

    added locators for Inscriere page

---
+++ SeleniumTestsProject/PageObjects/InscrierePage.Locators.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumTestsProject.PageObjects
+{
+    partial class InscrierePage
+    {
+        private IWebDriver _driver;
+
+        public InscrierePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public IWebElement NumeField => _driver.FindElement(By.XPath("//input[@name='name']"));
+        public IWebElement EmailField => _driver.FindElement(By.XPath("//input[@name='email']"));
+        public IWebElement TelefonField => _driver.FindElement(By.XPath("//input[@name='phone']"));
+        public IWebElement AdresaField => _driver.FindElement(By.XPath("//textarea[@name='address']"));
+        public IWebElement ParolaField => _driver.FindElement(By.XPath("//input[@name='password_1']"));
+        public IWebElement RepetaParolaField => _driver.FindElement(By.XPath("//input[@name='password_2']"));
+        public IWebElement InscriereButton => _driver.FindElement(By.XPath("//button[@type='submit']"));
+    }
+}
